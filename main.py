9bffa7a9fd2eb98bd87cc85c97358d4ea1007168
MiguelVPrieto <miguelprieto.espanha@gmail.com>
2025-02-23 20:28:20 -0300

    preprocessing1

--- main.py
+++ main.py
@@ -7,5 +7,41 @@ from database import importData
 
 (x_train, y_train), (x_test, y_test) = importData()
 
+labels = [
+    "apple", "aquarium_fish", "baby", "bear", "beaver",
+    "bed", "bee", "beetle", "bicycle", "bottle",
+    "bowl", "boy", "bridge", "bus", "butterfly",
+    "camel", "can", "castle", "caterpillar", "cattle",
+    "chair", "chimpanzee", "clock", "cloud", "cockroach",
+    "couch", "crab", "crocodile", "cup", "dinosaur",
+    "dolphin", "elephant", "flatfish", "forest", "fox",
+    "girl", "hamster", "house", "kangaroo", "keyboard",
+    "lamp", "lawn_mower", "leopard", "lion", "lizard",
+    "lobster", "man", "maple_tree", "motorcycle", "mountain",
+    "mouse", "mushroom", "oak_tree", "orange", "orchid",
+    "otter", "palm_tree", "pear", "pickup_truck", "pine_tree",
+    "plain", "plate", "poppy", "porcupine", "possum",
+    "rabbit", "raccoon", "ray", "road", "rocket",
+    "rose", "sea", "seal", "shark", "shrew",
+    "skunk", "skyscraper", "snail", "snake", "spider",
+    "squirrel", "streetcar", "sunflower", "sweet_pepper", "table",
+    "tank", "telephone", "television", "tiger", "tractor",
+    "train", "trout", "tulip", "turtle", "wardrobe",
+    "whale", "willow_tree", "wolf", "woman", "worm"
+]
+
+
 print("Training data shape:", x_train.shape)
 print("Test data shape:", x_test.shape)
+
+print(y_train[0])
+
+fig = plt.figure(figsize=(12, 8))
+columns = 5
+rows = 3
+for i in range(1, columns*rows +1):
+  img = x_train[i]
+  fig.add_subplot(rows, columns, i)
+  plt.title(labels[y_train[i][0]])
+  plt.imshow(img, cmap='binary')
+plt.show()
---
+++ model.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+def buildBaselineModel():
+    model = tf.keras.Sequential()
+
+    return model
+
+
+def buildBetterModel():
+    model = tf.keras.Sequential()
+
+    return model
+
+def trainModel(model):
+    return 0
